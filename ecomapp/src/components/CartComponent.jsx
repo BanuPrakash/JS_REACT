bde764aa6e1d6811ee9c66c8a4c71bfc71b16031
Banuprakash <banuprakashc@yahoo.co.in>
2025-09-01 15:20:50 +0530

    CartContext

--- ecomapp/src/components/CartComponent.jsx
+++ ecomapp/src/components/CartComponent.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { CartContext } from '../context/CartContextProvider'
+import { Button } from 'react-bootstrap'
+import CartRow from './CartRow';
 
 export default function CartComponent() {
+  let { cartItems, total, clearCart } = React.useContext(CartContext)
   return (
-    <div>CartComponent</div>
+    <div>
+      {
+        cartItems.map(item => <CartRow product={item} key={item.id}/>)
+      }
+      <div className='row'>
+        <div className='col-md-8'>&nbsp;</div>
+        <div className='col-md-4'>Total {total}</div>
+      </div>
+      <div className='row'>
+        <div className='col-md-8'>&nbsp;</div>
+        <div className='col-md-4'>
+          <Button variant='success'>Checkout</Button>
+        </div>
+      </div>
+    </div>
+
   )
 }
