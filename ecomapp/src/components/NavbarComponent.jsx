f62e73f120b4be5823a4fd48d99163fa4a46d6bb
Banuprakash <banuprakashc@yahoo.co.in>
2025-09-01 14:32:30 +0530

    axios

--- ecomapp/src/components/NavbarComponent.jsx
+++ ecomapp/src/components/NavbarComponent.jsx
@@ -1,3 +1,5 @@
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -11,7 +13,9 @@ export default function NavbarComponent() {
           <Navbar.Brand href="#home">ACME Shop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Products</Nav.Link>
-            <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
+            <Nav.Link as={Link} to="/cart">
+             <FontAwesomeIcon icon={faCartShopping} color='white' />
+            </Nav.Link>
             <Nav.Link as={Link} to="/form">Product Form</Nav.Link>
           </Nav>
         </Container>
